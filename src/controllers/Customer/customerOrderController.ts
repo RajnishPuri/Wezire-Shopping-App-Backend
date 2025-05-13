2a315da52d50aee3caf7040b071de29244320d47
RajnishPuri <rajnishpuri15@gmail.com>
2025-05-13 12:22:37 +0530

    Customer Page Completed

--- src/controllers/Customer/customerOrderController.ts
+++ src/controllers/Customer/customerOrderController.ts
@@ -32,3 +32,39 @@ export const getCustomerOrders = async (req: AuthRequest, res: Response) => {
         return res.status(500).json({ error: "Internal server error" });
     }
 };
+
+export const getOrderById = async (req: AuthRequest, res: Response) => {
+    try {
+        const orderId = req.params.orderId;
+        const customerId = req.customer.customerId;
+        console.log("Customer ID:", customerId);
+        console.log("Order ID:", orderId);
+
+        if (!customerId) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+        const order = await prisma.order.findUnique({
+            where: { id: orderId },
+            include: {
+                orderItems: {
+                    include: {
+                        product: true,
+                    },
+                },
+            },
+        });
+
+        if (!order) {
+            return res.status(404).json({ error: "Order not found" });
+        }
+
+        if (order.customerId !== customerId) {
+            return res.status(403).json({ error: "Forbidden" });
+        }
+
+        return res.status(200).json({ order });
+    } catch (error) {
+        console.error("Error fetching order by ID:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
--- src/routes/customerRoutes/customerOrderRoutes.ts
+++ src/routes/customerRoutes/customerOrderRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getCustomerOrders } from '../../controllers/Customer/customerOrderController';
+import { getCustomerOrders, getOrderById } from '../../controllers/Customer/customerOrderController';
 import { authenticateCustomer } from '../../middlewares/userAuth.middleware';
 
 const router = express.Router();
 router.get('/getCustomerOrders', authenticateCustomer as any, getCustomerOrders as any);
+router.get('/getOrderById/:orderId', authenticateCustomer as any, getOrderById as any);
 
 export default router;
